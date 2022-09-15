4dc6b5b62ae7d2f48482a8f3767cdc5e1b23759d
Maria_A <cloudyexpressions@gmail.com>
2022-09-15 21:09:35 +0500

    Задача 054

---
+++ 054/Program.cs
@@ -0,0 +1,60 @@
+﻿Console.WriteLine("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите диапазон: от 1 до => ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Массив: ");
+int[,] array = new int[m, n];  
+NewArray(array);
+WriteArray(array);
+
+Console.WriteLine("Отсортированный массив: "); 
+SortArray(array);
+WriteArray(array);
+
+void SortArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+
+void NewArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(x);
+        }
+    }
+}
+
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
