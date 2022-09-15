c1f07bb1b6c8f78306618ef8652ad791e7171ccd
Maria_A <cloudyexpressions@gmail.com>
2022-09-16 01:01:39 +0500

    Задача 064

---
+++ 064/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+string Numbers(int start, int end)
+{
+    if (start >= end) return start.ToString();
+    return (start + ", " + Numbers(start + 1, end));
+}
+
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число N (больше M): ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Натуральные числа в промежутке от M до N: " + Numbers(m, n));
